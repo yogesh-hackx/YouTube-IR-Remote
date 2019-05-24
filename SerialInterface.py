1857ca791d7a1c1e6a14b7a7b0276a2f1dd81d0e
yogesh-hackx <yogesh.hackx@gmail.com>
2019-05-24 17:01:15 +0530

    Added Volume functionality.

--- SerialInterface.py
+++ SerialInterface.py
@@ -1,34 +1,78 @@
 import serial
 import pyautogui
+import subprocess as s
 
-arduinoSerialData = serial.Serial('/dev/ttyACM1', 9600)	#Replace ttyACM1 with your corresponding port
+arduinoSerialData = serial.Serial('/dev/ttyACM0', 9600)	#Replace ttyACM1 with your corresponding port
 
-while(1 == 1):
+#Control Functions
+def seekBack():
+	pyautogui.hotkey('shift', 'left')
+
+def seekForward():
+	pyautogui.hotkey('shift', 'right')
+
+def play():
+	pyautogui.press('space')
+
+def mute():
+	pyautogui.press('m')
+
+def captions():
+	pyautogui.press('c')
+
+def fullScreen():
+	pyautogui.press('f')
+
+def volumeDec():
+	s.Popen(['amixer', 'set', 'Master', '10%-'])
+
+def volumeInc():
+	volOutput = s.check_output(['amixer', 'set', 'Master', '10%+'])
+	volOutput = str(volOutput)
+	if ("off" in volOutput):
+		s.Popen(['amixer', '-D', 'pulse', 'set', 'Master', 'toggle'])
+		volOutput = s.check_output(['amixer', 'set', 'Master', '40%+'])
+def toggle():
+	pyautogui.hotkey('alt', 'tab')
+
+#Judgement Area :P
+while(True):
 	if(arduinoSerialData.inWaiting()>0):
 		myData = arduinoSerialData.readline()
 		plainstring1 = myData.decode("utf-8", "ignore")
-		print(plainstring1)
+		#print(plainstring1)							Remove this Comment to see your IR codes
 		if("33456255" in plainstring1):
 			print("Play/Pause")
-			pyautogui.keyDown('space')
-			pyautogui.keyUp('space')
+			play()
 
 		if("33439935" in plainstring1):
 			print("SeekBack")
-			pyautogui.hotkey('shift', 'left')
+			seekBack()
 
 		if("33472575" in plainstring1):
 			print("SeekForward")
-			pyautogui.hotkey('shift', 'right')
+			seekForward()
 
 		if("33446055" in plainstring1):
 			print("FullScreen_Toggle")
-			pyautogui.press('f')
+			fullScreen()
 
 		if("33460335" in plainstring1):
 			print("Captions_Toggle")
-			pyautogui.press('c')
-
+			captions()
+		
 		if("33454215" in plainstring1):
 			print("Mute_Toggle")
-			pyautogui.press('m')
+			mute()
+
+		if("33464415" in plainstring1):
+			print("volume 10% --")
+			volumeDec()
+
+		if("33448095" in plainstring1):
+			print("volume 10% ++")
+			volumeInc()
+
+		if("33441975" in plainstring1):
+			print("Toggle")
+			toggle()
